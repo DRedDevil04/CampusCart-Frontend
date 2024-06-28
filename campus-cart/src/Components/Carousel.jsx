011d3e50134358d32bafb0211075b0bfcebcbc88
Gamin8ing <anim8tor.ing@gmail.com>
2024-06-28 22:52:14 +0530

    add carousel

--- campus-cart/src/Components/Carousel.jsx
+++ campus-cart/src/Components/Carousel.jsx
@@ -1,39 +1,126 @@
 import React, { useRef } from "react";
 import "./styles.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-function Carousel(props) {
-  if (props.isInfinite) {
-    setTimeout(() => {
-      props.setCurrentImg((props.currentImg + 1) % props.srcs.length);
-    }, props.nextTime);
-  }
+import Slider from "react-slick";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
   return (
-    <>
-      <div className="img-cont-car">
-        <div className="img-car">
-          {props.srcs.map((src, index) => {
-            if (props.currentImg === index)
-              return (
-                <img
-                  key={index}
-                  src={src}
-                  alt="this"
-                  draggable="false"
-                  style={{ display: "inline-block" }}
-                />
-              );
-            return <img key={index} src={src} alt="this" draggable="false" />;
-          })}
-        </div>
-        <div className="left-arrow-car" onClick={props.prevImg}>
-          ❮
-        </div>
-        <div className="right-arrow-car" onClick={props.nextImg}>
-          ❯
-        </div>
-      </div>
-    </>
+    <div
+      className={className}
+      style={{
+        ...style,
+        content: "❮",
+        fontSize: "1.5rem",
+        width: "50px",
+        height: "50px",
+        backgroundColor: "#3f72af",
+        color: "green",
+        display: "flex",
+        right: "-60px",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: "100%",
+        cursor: "pointer",
+        transition: "all 0.2s",
+      }}
+      onClick={onClick}
+    />
   );
 }
 
-export default Carousel;
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        content: "❮",
+        fontSize: "1.5rem",
+        width: "50px",
+        height: "50px",
+        backgroundColor: "#3f72af",
+        color: "green",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: "100%",
+        cursor: "pointer",
+        transition: "all 0.2s",
+        left: "-60px",
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
+export default function Carousel(props) {
+  var settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+  return (
+    <Slider {...settings}>
+      {props.srcs.map((src, index) => {
+        return (
+          <div className="img-car">
+            <img
+              key={index}
+              src={src}
+              alt="this"
+              draggable="false"
+              style={{ display: "inline-block" }}
+            />
+          </div>
+        );
+      })}
+    </Slider>
+  );
+}
+
+// function Carousel(props) {
+//   if (props.isInfinite) {
+//     setTimeout(() => {
+//       props.setCurrentImg((props.currentImg + 1) % props.srcs.length);
+//     }, props.nextTime);
+//   }
+//   return (
+//     <>
+//       <div className="img-cont-car">
+//         <div className="img-car">
+//           {props.srcs.map((src, index) => {
+//             if (props.currentImg === index)
+//               return (
+//                 <img
+//                   key={index}
+//                   src={src}
+//                   alt="this"
+//                   draggable="false"
+//                   style={{ display: "inline-block" }}
+//                 />
+//               );
+//             return <img key={index} src={src} alt="this" draggable="false" />;
+//           })}
+//         </div>
+//         <div className="left-arrow-car" onClick={props.prevImg}>
+//           ❮
+//         </div>
+//         <div className="right-arrow-car" onClick={props.nextImg}>
+//           ❯
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Carousel;
--- campus-cart/src/Pages/shop.js
+++ campus-cart/src/Pages/shop.js
@@ -4,16 +4,41 @@ import Card from "../Components/Card";
 import { IoIosAddCircle, IoIosCloseCircle } from "react-icons/io";
 import AddProd from "../Components/AddProd";
 import { useNavigate } from "react-router-dom";
+import Carousel from "../Components/Carousel";
 
 function App(props) {
   var isAdmin = true; // TODO: adding props.isAdmin
   var [isVisible, setIsVisible] = useState(false);
   const navigate = useNavigate();
 
-  const handler = ()=>{
+  //----------------- carousel -------------------------
+  const srcs = [
+    "https://www.nykaa.com/beauty-blog/wp-content/uploads/images/issue283/8-Breakthrough-Products-That-Are-Selling-Faster-Than-You-Can-Cou_OI.jpg",
+    "https://cdn.shopify.com/s/files/1/0070/7032/files/trending-products_c8d0d15c-9afc-47e3-9ba2-f7bad0505b9b.png?v=1614559651",
+    "https://hips.hearstapps.com/hmg-prod/images/gh-best-skincare-products-6557978b58b57.png",
+    "https://www.nowfoods.com/sites/default/files/styles/masthead_64/public/2023-11/Natrual_Foods_Hero-2_0.jpg?itok=WYhBCrrj",
+    "https://cdn.shopify.com/s/files/1/1859/8979/files/image17_3cfc7cfb-8215-40b7-a297-db7285d5375b.png?v=1610384290",
+    "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-madebymath-90946.jpg&fm=jpg",
+  ];
+
+  const [currentImg, setCurrentImg] = useState(0);
+  function nextImg() {
+    console.log("next");
+    setCurrentImg((currentImg + 1) % srcs.length);
+  }
+  function prevImg() {
+    console.log("prev");
+    setCurrentImg(
+      (currentImg - 1 < 0 ? currentImg - 1 + srcs.length : currentImg - 1) %
+        srcs.length
+    );
+  }
+  //---------------------------------------------------
+
+  const handler = () => {
     console.log("clicked");
     navigate("/product");
-  }
+  };
 
   return (
     <>
@@ -24,6 +49,18 @@ function App(props) {
         isAdmin={isAdmin}
       />
       <section className="page">
+        <div className="img-cont-car">
+          <Carousel
+            srcs={srcs}
+            currentImg={currentImg}
+            setCurrentImg={setCurrentImg}
+            isInfinite={true}
+            nextTime={5000}
+            nextImg={nextImg}
+            prevImg={prevImg}
+          />
+        </div>
+        <h1 className="text-hover">Our products</h1>
         <div className="container">
           {isAdmin ? (
             <div
