793c7e456dc917c2006a4d6ab16bf4453eb9a880
Gamin8ing <anim8tor.ing@gmail.com>
2024-07-04 15:18:59 +0530

    finalize tweaks

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,9 +13,10 @@ import {
   MenuItem,
   MenuDivider,
   useDisclosure,
+  Tooltip,
 } from "@chakra-ui/react";
-import LogoutModal from "./logout.jsx"; // Ensure correct import path and component name
-import { useNavigate,useLocation } from "react-router-dom";
+import LogoutModal from "./Logout.jsx"; // Ensure correct import path and component name
+import { useNavigate, useLocation } from "react-router-dom";
 import { IoMenu } from "react-icons/io5";
 import { selectUser } from "../slices/authSlice.js";
 import { useSelector } from "react-redux";
@@ -23,21 +24,16 @@ import { TiShoppingCart } from "react-icons/ti";
 
 const Header = ({ onOpen }) => {
   const navigate = useNavigate();
-  const location=useLocation();
+  const location = useLocation();
   const [isNonMobile] = useMediaQuery("(min-width: 420px)");
   const { isOpen, onOpen: onOpenModal, onClose } = useDisclosure();
-  const userInfo=useSelector(selectUser);
-  const isAdmin=userInfo.role==='admin';
-  const isShopPage=location.pathname==='/shop';
+  const userInfo = useSelector(selectUser);
+  const isAdmin = userInfo.role === "admin";
+  const isShopPage = location.pathname === "/shop";
 
   return (
     <Box minW="320px" w="100%" className="Header" bg={"#3f72af"} p={4}>
-      <Flex
-        align="center"
-        display={"flex"}
-        justifyContent="space-between"
-        // position={"relative"}
-      >
+      <Flex align="center" display={"flex"} justifyContent="space-between">
         <IconButton
           colorScheme="whiteAlpha"
           aria-label="Open Sidebar"
@@ -60,47 +56,46 @@ const Header = ({ onOpen }) => {
         >
           CampusCart
         </Heading>
-        
 
         <Flex alignItems="center" gap="1rem">
           {isShopPage && (
-            <IconButton bg="none"
-            _hover={{ bg: "#3f72af" ,transform:'scale(1.1)'}}
-             onClick={()=>navigate('/cart')}
-          >
-            <TiShoppingCart size="md" color="white"/>
-          </IconButton>
-          )}          
+            <Tooltip label="My Cart" aria-label="Cart">
+              <IconButton
+                bg="none"
+                _hover={{ bg: "#3f72af", transform: "scale(1.1)" }}
+                onClick={() => navigate("/cart")}
+              >
+                <TiShoppingCart size="md" color="white" />
+              </IconButton>
+            </Tooltip>
+          )}
           <Flex
-          alignItems="center"
-          flexDirection={{base:"column-reverse",lg:"row"}}
-          gap="2"
-          whiteSpace={"nowrap"}
-          //pr={{base:"1rem"}}
-          // position={"absolute"}
-          // right={0}
+            alignItems="center"
+            flexDirection={{ base: "column-reverse", lg: "row" }}
+            gap="2"
+            whiteSpace={"nowrap"}
           >
-          <Text
-            fontSize={["sm", "md", "xl", "2xl"]}
-            color={"#f9f7f7"}
-            fontWeight="500"
-          >
-            {userInfo.name.substring(0,5)}..
-          </Text>
-          <Menu>
-            <MenuButton
-              as={Avatar}
-              cursor={"pointer"}
-              name="Admin"
-              src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-              size="md"
-            />
-            <MenuList>
-              <MenuItem>View profile</MenuItem>
-              <MenuDivider />
-              <LogoutModal />
-            </MenuList>
-          </Menu>
+            <Text
+              fontSize={["sm", "md", "xl", "2xl"]}
+              color={"#f9f7f7"}
+              fontWeight="500"
+            >
+              {userInfo.name.substring(0, 5)}..
+            </Text>
+            <Menu>
+              <MenuButton
+                as={Avatar}
+                cursor={"pointer"}
+                name="Admin"
+                src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+                size="md"
+              />
+              <MenuList>
+                <MenuItem>View profile</MenuItem>
+                <MenuDivider />
+                <LogoutModal />
+              </MenuList>
+            </Menu>
           </Flex>
         </Flex>
       </Flex>
